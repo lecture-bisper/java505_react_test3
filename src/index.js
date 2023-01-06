9215ca24209f1d2d41ce1667f7c02423037a9b25
lecture-bisper <bisper@outlook.kr>
2023-01-06 16:42:29 +0900

    리액트 라우터 라이브러리 추가.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App3 from "./App3";
 import App4 from "./App4";
 import App5 from "./App5";
 import App6 from "./App6";
+import App7 from "./App7";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,7 +20,8 @@ root.render(
     {/*<App3 />*/}
     {/*<App4 />*/}
     {/*<App5 />*/}
-    <App6 />
+    {/*<App6 />*/}
+    <App7 />
 
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
   </React.StrictMode>
